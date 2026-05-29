4290044e19fb9f8122f3afc7cdb2edc5454e6864
hyperz111 <firzeroun11@gmail.com>
2026-05-29 19:15:46 +0700

    chore: update styles

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 	let totals = $state({});
 </script>
 
-<header class="text-center">
+<header class="text-center my-4">
 	<h1 class="text-5xl font-extrabold text-green-400">Atur MBG</h1>
 	<p class="text-gray-500">Alat untuk mengatur isi ompreng MBG kalian. Bisa dipakai untuk analisa.</p>
 </header>
